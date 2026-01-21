8447fdbbaa924133a861c1ec1d2fa162b6bcd0d3
Fayth <phanquancanhhao@gmail.com>
2026-01-21 17:23:59 +0700

    Added rpg.tsx

---
+++ app/rpg.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+
+type Player = {
+    EXP: number;
+    level: number;
+    health: number;
+    mana: number;
+}
+
+export default function Rpg() {
+  return <Text>RPG Screen</Text>;
+}
